3ed796e3fbfe9438ebf58ccfc9a52619275c103e
Sira5 <sahanmerve@hotmail.com>
2019-02-04 14:21:42 +0300

    User class oluşturma

---
+++ TeamProject/TeamProject/Models/UserLogin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TeamProject.Models
+{
+    
+    public class UserLogin
+    {
+        [Key]
+        public int UserId { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string UserName { get; set;}
+        [Required]
+        [MaxLength(16)]
+        public string Password { get; set; }
+        [Required]
+        [MaxLength(150)]
+        public string EPosta { get; set; }
+    }
+}
