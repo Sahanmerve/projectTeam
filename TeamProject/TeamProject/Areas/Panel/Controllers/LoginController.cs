cb0b4fc827b9daa0c201814f3e09c412a24103ce
Oğuzhan DERECİ <oguzz.dereci@gmail.com>
2019-02-06 14:57:14 +0300

    giris

---
+++ TeamProject/TeamProject/Areas/Panel/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TeamProject.Areas.Panel.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Panel/Login
+        public ActionResult Index()
+        {
+         
+            return View();
+
+        }
+       [HttpPost]
+        public JsonResult LoginControl(string name, string password)
+        {
+            if (name == "admin" && password == "123")
+            {
+                return Json(true);
+             
+            }
+            return Json(false);
+        }
+    }
+}
