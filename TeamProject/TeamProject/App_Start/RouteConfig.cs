662719532a877e1760ef4dc973119d9ffb4ff5e2
Oğuzhan DERECİ <oguzz.dereci@gmail.com>
2019-02-07 14:46:52 +0300

    açılış sayfası

--- TeamProject/TeamProject/App_Start/RouteConfig.cs
+++ TeamProject/TeamProject/App_Start/RouteConfig.cs
@@ -16,8 +16,8 @@ namespace TeamProject
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional }
+            ).DataTokens["area"] = "Panel";
         }
     }
 }
