749cfa700e1629d11baea9da11c1ba6789b0eb8c
Oğuzhan DERECİ <oguzz.dereci@gmail.com>
2019-02-11 14:19:12 +0300

    ekleme

--- TeamProject/TeamProject/Controllers/HomeController.cs
+++ TeamProject/TeamProject/Controllers/HomeController.cs
@@ -13,19 +13,19 @@ namespace TeamProject.Controllers
         int Id;
         public ActionResult Index()
         {
+            
             Id = (int)Session["id"];
             var a= db.UserDetails.FirstOrDefault(x=> x.UserDetailID == Id);
             return View(a);
         }
-
+        
         public ActionResult _Conversation(string model)
         {
             Id = (int)Session["id"];
-            //var a = db.Messages.FirstOrDefault(x => x.UserID == Id);
+            var a = db.Messages.FirstOrDefault(x => x.UserID == Id);
             MessagesViewModel data = new MessagesViewModel();
             data.Messages = db.Messages.Where(x => x.UserID == Id).ToList();
             return View(data);
         }
-      
     }
 }
